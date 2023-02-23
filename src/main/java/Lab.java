69592669efd40d6b7d5a5d107c5360f38648637c
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-23 20:10:31 +0000

    Complete EvenorOdd assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,9 @@ public class Lab {
      * @return a String "Even" if n is even, and "Odd" of in is odd. The test cases are case-sensitive.
      */
     public String decide(int n){
-        return "";
+        if(n % 2 == 0){
+            return "Even";
+        } else 
+        return "Odd";
     }
 }
